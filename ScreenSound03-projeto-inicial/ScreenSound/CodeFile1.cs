8c08151bb5d592ddafc2cb4c1c268783f840e8fe
ViniciusVieiraJS <silvyo.vieira@sptech.school>
2024-02-27 14:18:03 -0300

    Atualizar código

---
+++ ScreenSound03-projeto-inicial/ScreenSound/CodeFile1.cs
@@ -0,0 +1,74 @@
+﻿public class Calculadora
+{
+    public double Calcular(double numero1, double numero2, char operacao)
+    {
+        Calculadora minhaCalculadora = new Calculadora();
+        double resultado = 0;
+
+        switch (operacao)
+        {
+            case '+':
+                resultado = minhaCalculadora.Somar(numero1, numero2);
+                break;
+            case '-':
+                resultado = Subtrair(numero1, numero2);
+                break;
+            case '*':
+                resultado = Multiplicar(numero1, numero2);
+                break;
+            case '/':
+                resultado = Dividir(numero1, numero2);
+                break;
+            case '^':
+                resultado = Potencia(numero1, numero2);
+                break;
+            case 'r':
+                resultado = RaizQuadrada(numero1);
+                break;
+            default:
+                Console.WriteLine("Operação inválida.");
+                break;
+        }
+
+        return resultado;
+    }
+
+    private double Somar(double a, double b)
+    {
+        Calculadora minhaCalculadora = new Calculadora();
+        return a + b;
+    }
+
+    private static double Subtrair(double a, double b)
+    {
+        return a - b;
+    }
+
+    private static double Multiplicar(double a, double b)
+    {
+        return a * b;
+    }
+
+    private static double Dividir(double a, double b)
+    {
+        if (b != 0)
+            return a / b;
+        else
+        {
+            Console.WriteLine("Erro: Divisão por zero.");
+            return 0;
+        }
+    }
+
+    private static double Potencia(double a, double b)
+    {
+        return Math.Pow(a, b);
+    }
+
+    private static double RaizQuadrada(double a)
+    {
+        return Math.Sqrt(a);
+    }
+
+
+}
---
+++ ScreenSound03-projeto-inicial/ScreenSound/Modelos/Avaliacao.cs
@@ -0,0 +1,20 @@
+﻿
+namespace ScreenSound.Modelos;
+
+    internal class Avaliacao
+    {
+        public int Nota { get; }
+        public Avaliacao(int nota)
+        {
+            Nota = nota;
+        }
+    // Nesta aula introduzimos uma nova classe Avaliacao para que ela pudesse isolar
+    // tanto o dado primitivo com o valor da nota (usamos um inteiro para representá-la)
+    // quanto possíveis comportamentos do conceito de avaliação em nossa aplicação
+    // (por exemplo a capacidade de transformar um texto em um objeto através do método estático Parse()
+    public static Avaliacao Parse(string texto)
+    {
+        int nota = int.Parse(texto);
+        return new Avaliacao(nota);
+    }
+    }
